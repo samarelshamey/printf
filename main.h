1d47c0af3ca282899ab4b093b5c10e179710bc74
marwanmohamed21346 <marwanmohamed2238@gmail.com>
2023-09-16 14:21:29 +0300

    update main.h for flags

--- main.h
+++ main.h
@@ -10,8 +10,19 @@
 
 #define BUFF_SIZE 1024
 
+
+/*flags*/
+#define F_PLUS 2
+#define F_MINUS 1
+#define F_SPACE 16
+#define F_HASH 8
+#define F_ZERO 4
+
 void _puts(char *s);
 int _putchar(int c);
 int _printf(const char *format, ...);
+
+/*get_flags*/
+int get_flags(const char *format, int *i);
 
 #endif
