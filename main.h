f80e942048c707f7275a8008e8499115a7aa30ea
samarelshamey <smsmelshamey90@gmail.com>
2023-09-16 02:06:58 -0700

    headerfile

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024
+
+void _puts(char *s);
+int _putchar(int c);
+int _printf(const char *format, ...);
+
+#endif
